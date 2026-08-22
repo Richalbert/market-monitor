be00bb761d2931593584fe229b07b3998a535248
Richalbert <richalbert@free.fr>
2026-08-22 15:01:45 +0200

    Add eBay OAuth token request

--- market_monitor/clients/ebay.py
+++ market_monitor/clients/ebay.py
@@ -1,6 +1,6 @@
 # =============================================================================
 # Project     : Market Monitor
-# File        : tests/clients/ebay.py
+# File        : market_monitor/clients/ebay.py
 # Author      : Richalbert
 # Created     : 2026-08-21
 # Last Update : 
@@ -8,12 +8,62 @@
 # Description : 
 #             
 # License     : MIT
-#==============================================================================
+# ==============================================================================
 
 from market_monitor.credentials.ebay import EbayCredentials
 
+import base64
 
-class EbayClient(EbayCredentials):
+class EbayClient:
     
-    def __init__(self, credentials: EbayCredentials):
+    def __init__(
+        self, 
+        credentials: EbayCredentials,
+        http_client=None,
+    ):
         self.credentials = credentials
+        self.http_client = http_client
+
+
+    def get_access_token(self)-> str:
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": self._build_authorization_header(),
+        }
+
+        data = {
+            "grant_type": "client_credentials",
+            "scope": "https://api.ebay.com/oauth/api_scope",
+        }
+
+        response = self.http_client.post(
+            "https://api.sandbox.ebay.com/identity/v1/oauth2/token",
+            headers=headers,
+            data=data,
+        )
+
+        return response["access_token"]
+
+    def _build_authorization_header(self) -> str:
+
+        # Construction des identifiants client_id:client_secret
+        client_credentials = (
+            f"{self.credentials.client_id}:"
+            f"{self.credentials.client_secret}"
+        )
+
+        # Conversion str -> bytes
+        credentials_bytes = client_credentials.encode()
+
+        # Encodage Base64 : retourne des bytes
+        credentials_base64_bytes = base64.b64encode(credentials_bytes)
+
+        # Conversion bytes -> str
+        credentials_base64 = credentials_base64_bytes.decode()
+
+        # Construction de la valeur du header Authorization
+        return f"Basic {credentials_base64}"
+
+
+
--- tests/test_ebay_client.py
+++ tests/test_ebay_client.py
@@ -16,6 +16,11 @@
 from market_monitor.credentials.ebay import EbayCredentials
 from market_monitor.clients.ebay import EbayClient
 
+# === Test 13 ======================================
+#
+#   EbayClient conserve ses credentials
+#
+# --------------------------------------------------
 
 def test_ebay_client_stores_credentials():
 
@@ -30,5 +35,304 @@ def test_ebay_client_stores_credentials():
 
 
 
+# === Test 14 =====================================
+#
+#   EbayClient recupere l'access_token
+#
+# EbayClient demande un token a un composant HTTP et 
+# recupere l'access_token contenu dans la reponse
+#
+# Et le Stub simulera la reponse HTTP d'eBay
+#
+# EbayClient 
+#   -> appelle un client HTTP fictif
+#   -> recoit une reponse contenant access_token
+#   -> retourne "fake-access-token"
+# ---------------------------------------------------
+
+class StubHttpClient:
+
+    def post(self, url, headers=None, data=None):
+        return {
+            "access_token": "fake-access-token",
+            "expires_in": 7200,
+            "token_type": "Application Access Token",
+        }
 
 
+def test_ebay_client_gets_access_token():
+
+    # on fixe des IDs
+    credentials = EbayCredentials(
+        client_id="fake-client-id",
+        client_secret="fake-client-secret",
+    )
+
+    # on appelle le Stub
+    http_client = StubHttpClient()
+
+    # on fournit au client les IDs et la requete de connection
+    client = EbayClient(
+        credentials=credentials,
+        http_client=http_client,
+    )
+
+    # on recupere l'access_token
+    token = client.get_access_token()
+
+    # le contrat de ce test
+    assert token == "fake-access-token"
+
+# ==================================================
+
+
+# === Test 15 =================================
+#
+#   EbayClient utilise le bon endpoint OAuth
+# 
+#   Test: le client eBay utilise la bonne URL OAuth Sandbox
+#
+# Stub evolue en Spy
+#
+# EbayClient
+#   -> appelle le client HTTP
+#   -> a la bonne URL OAuth Sandbox
+# ---------------------------------------------
+
+class SpyHttpClient:
+
+    def __init__(self):
+        self.last_url = None
+
+    def post(self, url, headers=None, data=None):
+        self.last_url = url
+
+        return {
+            "access_token": "fake-access-token",
+            "expires_in": 7200,
+            "token_type": "Application Access Token",
+        }
+
+
+def test_ebay_client_uses_oauth_sandbox_url():
+        
+    # on fixe des IDs
+    credentials = EbayCredentials(
+        client_id="fake-client-id",
+        client_secret="fake-client-secret",
+    )
+    
+    # on appelle le Spy
+    http_client = SpyHttpClient()
+
+    # on fournit au client les IDs et la requete de connection
+    client = EbayClient(
+        credentials=credentials,
+        http_client=http_client,
+    )
+
+    # on recupere l'access_token la reponse a la requete
+    token = client.get_access_token()
+        
+    # le contrat de ce test
+    assert http_client.last_url == (
+        "https://api.sandbox.ebay.com/identity/v1/oauth2/token"
+    )
+    assert token == "fake-access-token"
+
+
+# === Test 16 =======================================
+#
+#   EbayClient envoie le bon Content-Type
+#
+# ----------------------------------------------------
+
+class SpyHttpClient:
+
+    def __init__(self):
+        self.last_url = None
+        self.last_headers = None
+
+    def post(self, url, headers=None, data=None):
+        self.last_url = url
+        self.last_headers=headers
+
+        return {
+            "access_token": "fake-access-token",
+            "expires_in": 7200,
+            "token_type": "Application Access Token",
+        }
+
+def test_ebay_client_uses_form_urlencoded_content_type():
+
+    credentials = EbayCredentials(
+        client_id="fake-client-id",
+        client_secret="fake-client-secret",
+    )
+
+    http_client = SpyHttpClient()
+
+    client = EbayClient(
+        credentials=credentials,
+        http_client=http_client,
+    )
+
+    token = client.get_access_token()
+
+    # le contrat de ce test
+    assert http_client.last_headers["Content-Type"] == (
+        "application/x-www-form-urlencoded"
+    )
+
+
+
+
+# === Test 17 ===========================================
+#
+#   EbayClient envoie Authorization Basic correctement
+#
+# --------------------------------------------------------
+
+class SpyHttpClient:
+
+    def __init__(self):
+        self.last_url = None
+        self.last_headers = None
+
+    def post(self, url, headers=None, data=None):
+        self.last_url = url
+        self.last_headers=headers
+
+        return {
+            "access_token": "fake-access-token",
+            "expires_in": 7200,
+            "token_type": "Application Access Token",
+        }
+
+def test_ebay_client_uses_basic_authorization():
+
+    credentials = EbayCredentials(
+        client_id="fake-client-id",
+        client_secret="fake-client-secret",
+    )
+
+    http_client = SpyHttpClient()
+
+    client = EbayClient(
+        credentials=credentials,
+        http_client=http_client,
+    )
+
+    token = client.get_access_token()
+
+    """ 
+        Pour eviter une erreur de calcul a la main, on demande a Python de realiser l'encodage 
+    
+            python -c 'import base64; \
+            print(base64.b64encode(b"fake-client-id:fake-client-secret").decode())'
+    """
+    assert http_client.last_headers["Authorization"] == (
+        "Basic ZmFrZS1jbGllbnQtaWQ6ZmFrZS1jbGllbnQtc2VjcmV0"
+    )
+
+
+
+
+
+# === Test 18 ============================================
+#
+#   EbayyClient envoie le body de la requete OAuth
+#   en commencant par grant_type=client_credentials
+#   puis le scope
+#
+# ---------------------------------------------------------
+
+class SpyHttpClient:
+
+    def __init__(self):
+        self.last_url = None
+        self.last_headers = None
+        self.last_data = None
+
+    def post(self, url, headers=None, data=None):
+        self.last_url = url
+        self.last_headers = headers
+        self.last_data = data
+
+        return {
+            "access_token": "fake-access-token",
+            "expires_in": 7200,
+            "token_type": "Application Access Token",
+        }
+
+def test_ebay_client_uses_client_credentials_grant_type():
+
+    credentials = EbayCredentials(
+        client_id="fake-client-id",
+        client_secret="fake-client-secret",
+    )
+
+    http_client = SpyHttpClient()
+
+    client = EbayClient(
+        credentials=credentials,
+        http_client=http_client,
+    )
+
+    token = client.get_access_token()
+
+    # le contrat du test
+    assert http_client.last_data["grant_type"] == "client_credentials"
+
+
+
+# === Test 19 ==================================================
+#
+#   La logique du scope OAuth, decrit ce que l'appli a le droit
+#   de faire avec le token obtenu
+#
+#   scope=https://api.ebay.com/oauth/api_scope
+# --------------------------------------------------------------
+
+class SpyHttpClient:
+
+    def __init__(self):
+        self.last_url = None
+        self.last_headers = None
+        self.last_data = None
+
+    def post(self, url, headers=None, data=None):
+        self.last_url = url
+        self.last_headers = headers
+        self.last_data = data
+
+        return {
+            "access_token": "fake-access-token",
+            "expires_in": 7200,
+            "token_type": "Application Access Token",
+        }
+
+
+def test_ebay_client_uses_api_scope():
+
+    credentials = EbayCredentials(
+        client_id="fake-client-id",
+        client_secret="fake-client-secret",
+    )
+
+    http_client = SpyHttpClient()
+
+    client = EbayClient(
+        credentials=credentials,
+        http_client=http_client,
+    )
+
+    token = client.get_access_token()
+
+
+
+
+    # le contrat du test
+    assert http_client.last_data["scope"] == (
+        "https://api.ebay.com/oauth/api_scope"
+    )
