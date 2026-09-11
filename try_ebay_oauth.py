92f51c8714a18ae097c812a281301e60f1d4e7ae
Richalbert <richalbert@free.fr>
2026-09-11 15:48:37 +0200

    Update eBay integration script for production

--- try_ebay_oauth.py
+++ try_ebay_oauth.py
@@ -7,8 +7,8 @@ from market_monitor.http.requests_client import RequestsHttpClient
 
 
 credentials = EbayCredentials(
-    client_id=os.environ["EBAY_CLIENT_ID"],
-    client_secret=os.environ["EBAY_CLIENT_SECRET"],
+    client_id=os.environ["EBAY_PRODUCTION_CLIENT_ID"],
+    client_secret=os.environ["EBAY_PRODUCTION_CLIENT_SECRET"],
 )
 
 http_client = RequestsHttpClient(requests)
@@ -16,6 +16,7 @@ http_client = RequestsHttpClient(requests)
 client = EbayClient(
     credentials=credentials,
     http_client=http_client,
+    base_url="https://api.ebay.com",
 )
 
 token = client.get_access_token()
@@ -24,9 +25,24 @@ print("Token recu :", bool(token))
 print("Longueur du token :", len(token))
 
 results = client.search(
-    query="iphone",
+    query="X570 UNIFY",
     access_token=token,
 )
 
 print("Cles de la reponse :", results.keys())
 print("Nombre de resultats :", results["total"])
+
+items = results.get("itemSummaries")
+
+if items:
+    item = items[0]
+
+    print("ID :", item.get("itemId"))
+    print("Titre :", item.get("title"))
+    print("Prix :", item.get("price"))
+    print("Etat :", item.get("condition"))
+    print("URL :", item.get("itemWebUrl"))
+    print("Marketplace :", item.get("listingMarketplaceId"))
+
+    for item in items:
+        print(item.get("title"), "-", item.get("price"))
