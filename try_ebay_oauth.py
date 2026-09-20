31e3f4fb71374eb91fa494378f3a860c07469894
Richalbert <richalbert@free.fr>
2026-09-20 15:27:04 +0200

    Simplify eBay Production Taxonomy Smoke Test

--- try_ebay_oauth.py
+++ try_ebay_oauth.py
@@ -6,12 +6,26 @@ from market_monitor.clients.ebay import EbayClient
 from market_monitor.http.requests_client import RequestsHttpClient
 
 
+# ----------------------------------------------
+# Recuperation des identifiants eBay Production
+# ----------------------------------------------
+
 credentials = EbayCredentials(
     client_id=os.environ["EBAY_PRODUCTION_CLIENT_ID"],
     client_secret=os.environ["EBAY_PRODUCTION_CLIENT_SECRET"],
 )
 
+
+# -----------------
+# Client HTTP reel
+# -----------------
+
 http_client = RequestsHttpClient(requests)
+
+
+# -----------------------
+# Client eBay Production
+# -----------------------
 
 client = EbayClient(
     credentials=credentials,
@@ -19,30 +33,19 @@ client = EbayClient(
     base_url="https://api.ebay.com",
 )
 
-token = client.get_access_token()
 
-print("Token recu :", bool(token))
-print("Longueur du token :", len(token))
+# ----------------------------------------------------
+# Suggestions de categories
+#
+# EbayClient effectue automatiquement :
+#
+#   1. l'obtention du token OAuth
+#   2. la recherche du categoryTreeId de EBAY_FR
+#   3. la recherche des suggestions Taxonomy
+# ----------------------------------------------------
 
-results = client.search(
+suggestions = client.get_category_suggestions(
     query="X570 UNIFY",
-    access_token=token,
 )
 
-print("Cles de la reponse :", results.keys())
-print("Nombre de resultats :", results["total"])
-
-items = results.get("itemSummaries")
-
-if items:
-    item = items[0]
-
-    print("ID :", item.get("itemId"))
-    print("Titre :", item.get("title"))
-    print("Prix :", item.get("price"))
-    print("Etat :", item.get("condition"))
-    print("URL :", item.get("itemWebUrl"))
-    print("Marketplace :", item.get("listingMarketplaceId"))
-
-    for item in items:
-        print(item.get("title"), "-", item.get("price"))
+print("Suggestions :", suggestions)
